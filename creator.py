d75b3b2c00f07c1ae230f41dab17878e019d5776
kandrewkov <47055688+kandrewkov@users.noreply.github.com>
2021-05-21 04:22:16 +0700

    Add files via upload

---
+++ creator.py
@@ -0,0 +1,16 @@
+class Creator:
+
+    def __init__():
+        
+    def fill_by_movies(name):
+
+    def get_answer():
+#   open json from user_test
+#   fill genres, country, and other binary features
+
+#   open json with rated movies from user
+# fill time, age, rating, 
+
+    answer = np.zeros(len(data))
+    
+        return answer
