5e42ad3db48e552a907e6aa3b405a7a206d994f0
kandrewkov <47055688+kandrewkov@users.noreply.github.com>
2021-05-21 18:19:09 +0700

    Прописал модель

--- creator.py
+++ creator.py
@@ -5,45 +5,83 @@ import json
 
 class Creator:
 
-    def __init__(answer=None, a=1, b=1):
+    def __init__(a=1, b=1):
         """
         def get_answer(a,b)
             return np.array = a*mean(movies) + b*test
         """
-        if answer is None:
-            self.answer = np.array(len(data))
-        else:
-            self.answer = answer
-            
         self.a = a
         self.b = b
-        # read file
+        
         with open('example.json', 'r') as myfile:
-            data=myfile.read()
-        # parse file
-        self.obj = json.loads(data)
+            jdata=myfile.read()
+        self.obj = json.loads(jdata) # parse file
+        
+#       подбор фильма не для себя
+        self.for_another = bool(self.obj['for_another'])
+        data = pd.read_csv('data.csv')
+        self.enum_columns = {anglphbt = {k: v for v, k in enumerate(data.columns)}
+        if self.for_anoter:
+            self.vector = np.zeros_like(data.iloc[1])
+            self.visited_indexs = []
+            self.rate_count = 0
+        else:
+            user_id = int(self.obj['user_id'])
+#             Загружать из бд
+            self.vector = None
+            self.visited_indexs = None
+            self.rate_count = None
+            
+        self.test_movies_id = self.obj['test_movies_id']
+        self.test_movies_rates = self.obj['test_movies_id']
+        self.liked_counties = self.obj['liked_counties']
+        self.disliked_counties = self.obj['disliked_counties']
+        self.liked_genres = self.obj['liked_counties']
+        self.disliked_genres = self.obj['disliked_counties']
+
         
     def fill_by_movies(movies_id):
          """open json with rated movies from user fill time, age, rating, """
+        
+        n = 0
         data = pd.read_csv('data.csv')
-        result = np.zeros(len(data.shape))
-        count = 0
-        data.iloc[id]
-        for id in movies_id:
+        result = np.zeros_like(self.vector)
+        
+        for id, rate in zip(self.test_movies_id, self.test_movies_rates):
             movie_vec = np.array(data.iloc[id])
-            n += rate
-            rate = obj['rate']
-            
             if rate > 3: 
-                result += rate * movie_vec
+                result += (rate-3) * movie_vec
+                n += rate-3
             else:
-                result -= rate * movie_vec
+                result -= (3-rate) * movie_vec
+                n += 3-rate
 
-        answer = result/count
+        answer = result/n
     
     def fill_by_test():
         """open json from user_test
         fill genres, country, and other binary features"""
+        
+        result = np.zeros_like(self.vector)
+        for genre in self.liked_genres:
+            ind = self.enum_columns[genre]
+            result[ind] = 1
+         
+        for genre in self.disliked_genres:
+            ind = self.enum_columns[genre]
+            result[ind] = -1
+        
+        for country in self.liked_countries:
+            ind = self.enum_columns[genre]
+            result[ind] = 1
+         
+        for genre in self.disliked_contries:
+            ind = self.enum_columns[genre]
+            result[ind] = -1
+                             
+        return result
+        
+                             
         
     def get_answer():
         """
@@ -51,6 +89,6 @@ class Creator:
         return np.array = a*mean(movies) + b*test
         """
         movies_id = []
-        self.answer = self.a * self.fill_by_movies(movies_id)
-        self.answer += self.b * self.fill_by_test()
+        self.vector = self.a * self.fill_by_movies(movies_id)
+        self.vector += self.b * self.fill_by_test()
         return self.answer/(self.a + self.b)
